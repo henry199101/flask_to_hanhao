29e4c8c82bca73b72b53df9f9a8c5722ddb2262f
henry199101 <331243543@qq.com>
2018-08-28 09:53:25 +0800

    8.28commit

--- hello.py
+++ hello.py
@@ -36,7 +36,6 @@ def index():
         form.name.data = ''
     return render_template('index.html', form=form, name=name)
 
-manager = Manager.(app)
 
 if __name__ == '__main__':
-	manager.run()
+    app.run(debug=True)
